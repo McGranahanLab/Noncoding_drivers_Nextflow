57830a554a3ef9b9130347c13c31effd248b5035
Maria Litovchenko <maria.litovchenko@gmail.com>
2023-09-25 15:56:32 +0100

    Streamlined check_input_arguments function

--- bin/custom_functions.R
+++ bin/custom_functions.R
@@ -49,8 +49,10 @@ acceptedChrNames <- c(c(1:24, 'X', 'Y'), paste0('chr', c(1:24, 'X', 'Y')))
 #' @return void
 #' @export
 check_file_existence_msg <- function(filePath) {
-  if (!file.exists(filePath)) {
-    stop('[', Sys.time(), '] File does not exist: ', filePath)
+  if (!is.null(filePath)) {
+    if (!file.exists(filePath)) {
+      stop('[', Sys.time(), '] File does not exist: ', filePath)
+    }
   }
 }
 
@@ -65,9 +67,11 @@ check_file_existence_msg <- function(filePath) {
 #' @return void
 #' @export
 check_min_int_msg <- function(value, value_name, min_value) {
-  if (value < min_value) {
-    stop('[', Sys.time(), '] Minimal value for the argument --', value_name, 
-         ' is ', min_value)
+  if (!is.null(value)) {
+    if (value < min_value) {
+      stop('[', Sys.time(), '] Minimal value for the argument --', value_name, 
+           ' is ', min_value)
+    }
   }
 }
 
@@ -80,45 +84,27 @@ check_min_int_msg <- function(value, value_name, min_value) {
 #' @return void
 #' @export
 check_input_arguments <- function(argsList, outputType = NULL) {
-  if (!is.null(argsList$inventory_patients)) {
-    lapply(argsList$inventory_patients, check_file_existence_msg)
-  }
-  if (!is.null(argsList$inventory_analysis)) {
-    lapply(argsList$inventory_analysis, check_file_existence_msg)
-  }
-  if (!is.null(argsList$inventory_blacklisted)) {
-    lapply(argsList$inventory_blacklisted, check_file_existence_msg)
-  }
-  if (!is.null(argsList$min_n_participants)) {
-    check_min_int_msg(argsList$min_n_participants, 'min_n_participants', 1)
-  }
-  if (!is.null(argsList$min_depth)) {
-    check_min_int_msg(argsList$min_depth, 'min_depth', 1)
-  }
-  if (!is.null(argsList$min_tumor_vac)) {
-    check_min_int_msg(argsList$min_tumor_vac, 'min_tumor_vac', 1)
-  }
-  if (!is.null(argsList$min_tumor_vaf)) {
-    check_min_int_msg(argsList$min_tumor_vaf, 'min_tumor_vaf', 0)
-  }
-  if (!is.null(argsList$max_germline_vaf)) {
-    check_min_int_msg(argsList$max_germline_vaf, 'max_germline_vaf', 0)
-  }
-  if (!is.null(argsList$max_germline_vac)) {
-    check_min_int_msg(argsList$max_germline_vac, 'max_germline_vac', 0)
-  }
-  if (!is.null(argsList$max_n_vars)) {
-    check_min_int_msg(argsList$max_n_vars, 'max_n_vars', 0)
-  }
-  if (!is.null(argsList$min_reg_len)) {
-    check_min_int_msg(argsList$min_reg_len, 'min_reg_len', 0)
-  }
-  if (!is.null(argsList$target_genome_path)) {
-    check_file_existence_msg(argsList$target_genome_path)
-  }
-  if (!is.null(argsList$chain)) {
-    check_file_existence_msg(argsList$chain)
-  }
+  lapply(argsList$inventory_patients, check_file_existence_msg)
+  lapply(argsList$inventory_analysis, check_file_existence_msg)
+  lapply(argsList$inventory_blacklisted, check_file_existence_msg)
+  check_min_int_msg(argsList$min_n_participants, 'min_n_participants', 1)
+  check_min_int_msg(argsList$min_depth, 'min_depth', 1)
+  check_min_int_msg(argsList$min_tumor_vac, 'min_tumor_vac', 1)
+  check_min_int_msg(argsList$min_tumor_vaf, 'min_tumor_vaf', 0)
+  check_min_int_msg(argsList$max_germline_vaf, 'max_germline_vaf', 0)
+  check_min_int_msg(argsList$max_germline_vac, 'max_germline_vac', 0)
+  check_min_int_msg(argsList$max_n_vars, 'max_n_vars', 0)
+  check_min_int_msg(argsList$min_reg_len, 'min_reg_len', 1)
+  check_file_existence_msg(argsList$target_genome_path)
+  check_file_existence_msg(argsList$chain)
+  
+  check_file_existence_msg(argsList$variants)
+  check_file_existence_msg(argsList$genomic_regions)
+  check_file_existence_msg(argsList$target_genome_chr_len)
+  check_file_existence_msg(argsList$gene_name_synonyms)
+  check_file_existence_msg(argsList$varanno_conversion_table)
+  
+  check_min_int_msg(argsList$cores, 'cores', 1)
   
   if (!is.null(argsList$output)) {
     if (!is.null(outputType)) {
@@ -133,10 +119,6 @@ check_input_arguments <- function(argsList, outputType = NULL) {
     } else {
       stop('[', Sys.time(), '] Please give outputType argument.')
     }
-  }
-  
-  if (!is.null(argsList$cores)) {
-    check_min_int_msg(argsList$cores, 'cores', 1)
   }
 }
 
