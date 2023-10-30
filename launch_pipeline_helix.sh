439e6f5a6d220d4c57d3f95ec55d1c125650fb59
Maria Litovchenko <maria.litovchenko@gmail.com>
2023-10-30 08:46:52 +0000

    Added temporary directory

--- launch_pipeline_helix.sh
+++ launch_pipeline_helix.sh
@@ -7,10 +7,14 @@
 #BSUB -o nextflow_YYYY-MM-DD.out
 #BSUB -J nextflow
 #BSUB -R "rusage[mem=8000] span[hosts=1]"
+#BSUB -M 8000
 #BSUB -n 1
 #BSUB -cwd "."
 
 module load lang/Java/19.0
 module load tools/singularity/3.8.3
 
-./nextflow-23.04.2-all run main.nf -profile genomicsEngland
+export TMPDIR=/re_scratch/mlitovchenko/
+mkdir -p $TMPDIR
+
+./nextflow-23.04.2-all run main.nf -profile genomicsEngland -resume
