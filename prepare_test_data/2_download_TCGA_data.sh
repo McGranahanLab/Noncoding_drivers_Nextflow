c0c36040f416230dedd36f44cfadd8b05a14694f
Maria Litovchenko <maria.litovchenko@gmail.com>
2023-11-16 14:06:44 +0000

    Added 1_create_FPKM_table_TCGA.R

--- prepare_test_data/2_download_TCGA_data.sh
+++ prepare_test_data/2_download_TCGA_data.sh
@@ -9,14 +9,20 @@
 # files.cases.primary_site in ["bronchus and lung"] and 
 # files.data_type in ["Allele-specific Copy Number Segment","Gene Expression Quantification","Masked Somatic Mutation"]
 
-Rscript --vanilla select_tumors_from_TCGA.R \
+SAMPLE_SHEET='gdc_sample_sheet_test_cancer_drivers_pipeline.tsv'
+MANIFEST='gdc_manifest_test_cancer_drivers_pipeline.txt'
+TEST_DATA_FOLDER='test_dataset/'
+FPKM_TABLE_PER_SAMPLE='../data/assets_raw/TCGA_FPKM_per_sample.csv'
+FPKM_TABLE_PER_TUMOR_SUBTYPE='../data/assets_raw/TCGA_FPKM_per_tumor_subtype.csv'
+
+Rscript --vanilla 0_select_tumors_from_TCGA.R \
         --sample_sheet gdc_sample_sheet.2023-11-15.tsv \
         --manifest gdc_manifest_20231115_125954.txt \
         --sample_type "Primary Tumor" \
         --data_type "Allele-specific Copy Number Segment" "Masked Somatic Mutation" "Gene Expression Quantification" \
         --n_samples 100 \
-        --output_sample_sheet gdc_sample_sheet_test_cancer_drivers_pipeline.tsv \
-        --output_manifest gdc_manifest_test_cancer_drivers_pipeline.txt
+        --output_sample_sheet $SAMPLE_SHEET \
+        --output_manifest $MANIFEST
 
 # download executable of GDC Data Transfer Tool
 wget https://gdc.cancer.gov/files/public/file/gdc-client_v1.6.1_Ubuntu_x64.zip
@@ -24,5 +30,14 @@ unzip gdc-client_v1.6.1_Ubuntu_x64.zip
 rm gdc-client_v1.6.1_Ubuntu_x64.zip
 
 # download the data for selected samples
-mkdir test_dataset/
-./gdc-client download -m gdc_manifest_test_cancer_drivers_pipeline.txt -d test_dataset/
+mkdir $TEST_DATA_FOLDER
+./gdc-client download -m $MANIFEST -d $TEST_DATA_FOLDER
+
+# produce FPKM tables: one containing expression data for every sample and one 
+# containing expression data per tumor subtype (median)
+Rscript --vanilla 1_create_FPKM_table_TCGA.R --sample_sheet $SAMPLE_SHEET \
+        --folder $TEST_DATA_FOLDER \
+        --data_type "Gene Expression Quantification" \
+        --tumor_subtype_column 'Project ID' --do_aggregate T \
+        --aggregate_name PANLUNG  --output_per_sample $FPKM_TABLE_PER_SAMPLE \
+        --output_per_tumor_subtype $FPKM_TABLE_PER_TUMOR_SUBTYPE
