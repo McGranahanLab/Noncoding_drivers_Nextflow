5f639e8261ba90f5000c61531c474e40d30c76a8
Maria Litovchenko <maria.litovchenko@gmail.com>
2023-12-04 21:11:22 +0000

    Fixed bugs in 8_filter_tiered_drivers.R which prevented proper filtering,  pushed filtering by synonymous rate to arguments

--- bin/8_filter_tiered_drivers.R
+++ bin/8_filter_tiered_drivers.R
@@ -43,6 +43,8 @@ Sys.sleep(sample(1:15, 1))
 source(paste0(srcDir, '/custom_functions.R'))
 
 # Libraries -------------------------------------------------------------------
+suppressWarnings(suppressPackageStartupMessages(library(argparse)))
+suppressWarnings(suppressPackageStartupMessages(library(data.table)))
 
 # Parse input arguments -------------------------------------------------------
 # create parser object
@@ -96,51 +98,50 @@ maxLocalMutRateQhelp <- paste('Maximum local mutation rate (value between',
 parser$add_argument("-mlmrq", "--max_local_mut_rate_q", required = F, 
                     type = 'double', default = 1, help = maxLocalMutRateQhelp)
 
-useSynHelp <- paste('Whether or not quantile of synonumous mutation rate',
-                    'should be used as genomic region - specific mutation',
-                    'rate for coding genomic regions.')
-parser$add_argument("-syn", "--use_synonumous_mut_rate", required = F, 
-                    type = 'character', default = 'F', help = useSynHelp,
-                    choices = c('T', 'F'))
-
 maxGrMutRateQhelp <- paste('Maximum genomic region - specific mutation rate',
                            'quantile (value between 0 and 1)')
 parser$add_argument("-mgmrq", "--max_gr_mut_rate_q", required = F, 
                     type = 'double', default = 1, help = maxGrMutRateQhelp)
+
+maxSybGrMutRateQhelp <- paste('Maximum genomic region - specific mutation ',
+                              'rate quantile (value between 0 and 1) ',
+                              'computed based on synonymous mutations only. ',
+                              'This filter will only be applied to coding ',
+                              'genomic regions.')
+parser$add_argument("-mgsmrq", "--max_gr_syn_mut_rate_q", required = F, 
+                    type = 'double', default = 1, help = maxSybGrMutRateQhelp)
 
 maxGrLenQhelp <- paste('Maximum quantile of genomic region length',
                        '(value between 0 and 1)')
 parser$add_argument("-mglq", "--max_gr_len_q", required = F, type = 'double',
                     default = 1, help = maxGrLenQhelp)
 
-removeOlfactoryHelp <- paste('Maximum quantile of genomic region length',
-                             '(value between 0 and 1)')
-parser$add_argument("-o", "--remove_olfactory", required = F, type = 'character',
-                    default = 1, help = removeOlfactoryHelp)
+removeOlfactoryHelp <- paste('Wherther or not genomic regions associated to ',
+                             'olfactory genes whould be removed.')
+parser$add_argument("-of", "--remove_olfactory", required = F, 
+                    type = 'character', default = 'T', choices = c('T', 'F'),
+                    help = removeOlfactoryHelp)
 
+parser$add_argument("-o", "--output", required = T, type = 'character',
+                    help = "Path to the output file")
 
-as.logical(args$use_synonumous_mut_rate)
-as.logical(args$remove_olfactory)
+args <- parser$parse_args()
+args$remove_olfactory <- as.logical(args$remove_olfactory)
+# check_input_arguments_postproc(args, outputType = 'file')
 
-codingGRid = list('CDS')
-min_n_soft_cod = 3
-min_n_soft_noncod = 2
-min_n_muts = 3
-min_n_patients = 3
-max_local_mut_rate_q = 0.99
-use_synonumous_mut_rate = T
-max_gr_mut_rate_q = 0.99
-max_gr_len_q = 0.99
+timeStart <- Sys.time()
+message('[', Sys.time(), '] Start time of run')
+printArgs(args)
 
 # Test arguments --------------------------------------------------------------
-args <- list(inventory_analysis = '../data/inventory/inventory_analysis_tcga_short.csv',
-             cancer_subtype = 'LUAD',
-             tiered_pvals = '../TEST/results/tables/tiered_drivers/assignedTier-LUAD--hg19.csv',
-             min_n_soft_cod = 3,
-             min_n_soft_noncod = 2, min_n_muts = 3, min_n_patients = 3,
-             max_local_mut_rate_q = 0.99, use_synonumous_mut_rate = T,
-             max_gr_mut_rate_q = 0.99, max_gr_len_q = 0.99, 
-             remove_olfactory = T)
+# args <- list(inventory_analysis = '../data/inventory/inventory_analysis_tcga_short.csv',
+#              cancer_subtype = 'LUAD',
+#              tiered_pvals = '../TEST/results/tables/tiered_drivers/assignedTier-LUAD--hg19.csv',
+#              min_n_soft_cod = 3,
+#              min_n_soft_noncod = 2, min_n_muts = 3, min_n_patients = 3,
+#              max_local_mut_rate_q = 1, max_gr_syn_mut_rate_q = 0.99, 
+#              max_gr_len_q = 0.99, remove_olfactory = T, 
+#              output = 'drivers-LUAD--hg19.csv')
 
 # Read in analysis inventory --------------------------------------------------
 analysisInv <- readAnalysisInventory(args$inventory_analysis)
@@ -209,14 +210,14 @@ if (args$min_n_patients > 0) {
 # Mark genes with high local mutational rate  -------------------------------
 if ('meanMutRateQuant_local' %in% colnames(tiered_pvals)) {
   if (args$max_local_mut_rate_q < 1) {
-    pass <- tiered_pvals$meanMutRateQuant < 100 * args$max_local_mut_rate_q &
-      !is.na(tiered_pvals$meanMutRateQuant_local)
+    pass <- tiered_pvals$meanMutRateQuant_local <= 100 * args$max_local_mut_rate_q |
+      is.na(tiered_pvals$meanMutRateQuant_local)
     tiered_pvals[!pass]$FILTER <- paste0(tiered_pvals[!pass]$FILTER,
                                      '; high local mut.rate')
     
     message('[', Sys.time(), '] Found ', sum(!pass), '(',
             round(100 * sum(!pass) / nrow(tiered_pvals), 2), '%) genomic ',
-            'regions which have too low number of mutated participants.')
+            'regions which have too high local mutation rate.')
     rm(pass)
   }
 } else {
@@ -228,90 +229,64 @@ if ('meanMutRateQuant_local' %in% colnames(tiered_pvals)) {
 }
 
 # Mark by genomic regions specific mutational rate/synonymous mut.rate --------
-if (args$use_synonumous_mut_rate & 
-    !'synMeanMutRateQuant' %in% colnames(tiered_pvals)) {
-  message('[', Sys.time(), '] ',
-}
-
-if (args$use_synonumous_mut_rate) {
-  
-  if (!c('synMeanMutRateQuant') %in% colnames(tiered_pvals)) {
-    msg <- 'Filtering based on mean genome region '
-    if (args$use_synonumous_mut_rate) {
-      msg <- paste(msg, 'SYNONYMOUS')
-    }
-    msg <- paste(msg, 'mutation rate quantile was not performed, because ',
-                 'column ', colToUse, ' was not found.')
-    message('[', Sys.time(), '] ', msg)
+if (args$max_gr_mut_rate_q < 1 | args$max_gr_syn_mut_rate_q < 1) {
+  # set a flag to determine, if filtering by synonymous mutations should be
+  # performed
+  is_cod <- tiered_pvals$gr_id %in% coding_gr_id
+  doSyn <- F
+  if (args$max_gr_syn_mut_rate_q < 1 & 
+      'synMeanMutRateQuant' %in% colnames(tiered_pvals)) {
+    doSyn <- T
+  }
+  if (args$max_gr_syn_mut_rate_q < 1 & !doSyn) {
+    message('[', Sys.time(), '] Filtering by synonymous mutation rate in ',
+            'coding regions will not be performed because column ',
+            'synMeanMutRateQuant was not found. Will use instead column ',
+            'meanMutRateQuant.')
   }
   
-  
-  
-  # coding regions
-  grMutRate <- unlist(tiered_pvals[, 'synMeanMutRateQuant', with = F])
-  # is.na(grMutRate) because a gene can be mutated, but have 0 synonymous muts
-  pass <- (tiered_pvals$gr_id %in% coding_gr_id) &
-    (is.na(grMutRate) | grMutRate < 100 * args$max_gr_mut_rate_q)
-  tiered_pvals[!pass]$FILTER <- paste0(tiered_pvals[!pass]$FILTER, 
-                                       '; high syn gr mut.rate')
-  
-  message('[', Sys.time(), '] Found ', sum(!pass), '(',
-          round(100 * sum(!pass) / nrow(tiered_pvals[gr_id %in% coding_gr_id]), 
-                2), '%) genomic regions which have too high synonymous ',
-          'mutation rate.')
-  rm(pass, grMutRate)
-  
-  # noncoding regions
-  grMutRate <- unlist(tiered_pvals[, 'meanMutRateQuant', with = F])
-  pass <- (!tiered_pvals$gr_id %in% coding_gr_id)  &
-    (!is.na(grMutRate) & grMutRate < 100 * args$max_gr_mut_rate_q)
-  tiered_pvals[!pass]$FILTER <- paste0(tiered_pvals[!pass]$FILTER, 
-                                       '; high genomic region mut.rate')
-  
-  message('[', Sys.time(), '] Found ', sum(!pass), '(',
-          round(100 * sum(!pass) / 
-                  nrow(tiered_pvals[!gr_id %in% coding_gr_id]), 2), 
-          '%) genomic regions which have too high gemonic ',
-          'mutation rate.')
-  rm(pass, grMutRate)
-  
-} else {
-  
-}
-
-colToUse <- 'meanMutRateQuant'
-filterName <-  '; high genomic region mut.rate'
-if (args$use_synonumous_mut_rate) {
-  colToUse <- 'synMeanMutRateQuant'
-  filterName <-  '; high syn gr mut.rate'
-}
-
-if (colToUse %in% colnames(tiered_pvals)) {
+  # Assign dummy column values which will be replaced with real ones in case
+  # max_gr_mut_rate_q or max_gr_syn_mut_rate_q is < 1
+  tiered_pvals[, meanMutRateQuant_cutOff := 110]
+  tiered_pvals[, meanMutRateQuant_toFltr := -1]
   if (args$max_gr_mut_rate_q < 1) {
-    
-    grMutRate <- unlist(tiered_pvals[, colToUse, with = F])
-    pass <- grMutRate < !is.na(grMutRate) & 100 * args$max_gr_mut_rate_q 
-    tiered_pvals[!pass]$FILTER <- paste0(tiered_pvals[!pass]$FILTER, 
-                                         filterName)
-    
-    message('[', Sys.time(), '] Found ', sum(!pass), '(',
-            round(100 * sum(!pass) / nrow(tiered_pvals), 2), '%) genomic ',
-            'regions which have too', gsub(';', '', filterName))
-    rm(pass)
+    tiered_pvals[, meanMutRateQuant_toFltr := meanMutRateQuant]
+    tiered_pvals[, meanMutRateQuant_cutOff := 100 * args$max_gr_mut_rate_q]
   }
-} else {
-  if (!colToUse %in% colnames(tiered_pvals)) {
-    msg <- 'Filtering based on mean genome region '
-    if (args$use_synonumous_mut_rate) {
-      msg <- paste(msg, 'SYNONYMOUS')
-    }
-    msg <- paste(msg, 'mutation rate quantile was not performed, because ',
-                 'column ', colToUse, ' was not found.')
-    message('[', Sys.time(), '] ', msg)
+  if (doSyn) {
+    tiered_pvals[is_cod]$meanMutRateQuant_toFltr <- tiered_pvals[is_cod]$synMeanMutRateQuant
+    tiered_pvals[is_cod]$meanMutRateQuant_cutOff <- 100 * args$max_gr_syn_mut_rate_q
   }
+  
+  pass <- tiered_pvals$meanMutRateQuant_toFltr <= tiered_pvals$meanMutRateQuant_cutOff |
+    is.na(tiered_pvals$meanMutRateQuant_toFltr)
+  tiered_pvals[!pass]$FILTER <- paste0(tiered_pvals[!pass]$FILTER,
+                                       '; high genomic region mut.rate')
+  tiered_pvals[, meanMutRateQuant_toFltr := NULL]
+  tiered_pvals[, meanMutRateQuant_cutOff := NULL]
+  if (doSyn) {
+    tiered_pvals[is_cod]$FILTER <- gsub('; high genomic region mut.rate',
+                                        '; high syn genomic region mut.rate', 
+                                        tiered_pvals[is_cod]$FILTER)
+    rm(is_cod)
+  }
+  
+  message('[', Sys.time(), '] Found ', sum(!pass), '(',
+          round(100 * sum(!pass) / nrow(tiered_pvals), 2), '%) genomic ',
+          'regions which have too high genomic region - specific ',
+          'mutation rate.')
+  if (doSyn) {
+    message('[', Sys.time(), '] Synonymous mutation rate was used in the ',
+            'filter above for coding regions')
+  }
+  rm(pass)
 }
 
 # Mark olfactory genes --------------------------------------------------------
+if (args$remove_olfactory & !'is_olfactory' %in% colnames(tiered_pvals)) {
+  message('[', Sys.time(), '] Filtering of olfactory genes will not be ',
+          'performed because column is_olfactory was not found.')
+}
 if (args$remove_olfactory & 'is_olfactory' %in% colnames(tiered_pvals)) {
   pass <- !tiered_pvals$is_olfactory
   tiered_pvals[!pass]$FILTER <- paste0(tiered_pvals[!pass]$FILTER,
@@ -319,20 +294,20 @@ if (args$remove_olfactory & 'is_olfactory' %in% colnames(tiered_pvals)) {
   
   message('[', Sys.time(), '] Found ', sum(!pass), '(',
           round(100 * sum(!pass) / nrow(tiered_pvals), 2), '%) genomic ',
-          'regions which are olfactory.')
+          'regions which are linked to olfactory genes.')
   rm(pass)
 }
 
 # Mark by quantile of length --------------------------------------------------
 if (args$max_gr_len_q < 1) {
-  pass <- !is.na(tiered_pvals$gr_lenQuant) &
-          tiered_pvals$gr_lenQuant < 100 * args$max_gr_len_q
+  pass <- is.na(tiered_pvals$gr_lenQuant) |
+          tiered_pvals$gr_lenQuant <= 100 * args$max_gr_len_q
   tiered_pvals[!pass]$FILTER <- paste0(tiered_pvals[!pass]$FILTER, 
                                        '; long gene')
   
   message('[', Sys.time(), '] Found ', sum(!pass), '(',
           round(100 * sum(!pass) / nrow(tiered_pvals), 2), '%) genomic ',
-          'regions which too long.')
+          'regions which are too long.')
   rm(pass)
 }
 
@@ -348,7 +323,8 @@ if (length(exprCols) > 0) {
     
     message('[', Sys.time(), '] Found ', sum(!pass), '(',
             round(100 * sum(!pass) / nrow(tiered_pvals), 2), '%) genomic ',
-            'regions which are not expressed in ', eCol, '.')
+            'regions which are not expressed in ', gsub('expr_in_', '', eCol),
+            '.')
     rm(pass)
   }
 } 
@@ -357,6 +333,25 @@ if (length(exprCols) > 0) {
 tiered_pvals[, FILTER := gsub('^PASS; ', '', FILTER)]
 
 # Print summary table of filtering --------------------------------------------
+filterStats <- as.data.table(table(tiered_pvals[FILTER != 'PASS']$FILTER))
+# add number of instances with assigned tier which was filtered out
+if (nrow(tiered_pvals[!is.na(tier) & FILTER != 'PASS']) > 0) {
+  filterStats <- merge(filterStats, 
+                       as.data.table(table(tiered_pvals[!is.na(tier) & 
+                                                          FILTER != 'PASS']$FILTER)),
+                       by = 'V1', all.x = T)
+  filterStats[is.na(N.y)]$N.y <- 0
+} else {
+  filterStats[, N.y := 0]
+}
+setnames(filterStats, c('V1', 'N', 'N.x', 'N.y'),
+         c('Filter', 'N genomic regions', 'N genomic regions', 
+           'N genomic regions with assigned tier'), skip_absent = T)
+filterStats <- filterStats[order(-`N genomic regions`)]
+filterStats[, `% genomic regions` := round(100 * `N genomic regions` / 
+                                             nrow(tiered_pvals), 1)]
+message(paste0(capture.output(knitr::kable(filterStats, format = "markdown")), 
+               collapse = '\n'))
 
 # [SAVE] Tiered results as a table --------------------------------------------
 colOrderToPrint <- c('tumor_subtype', 'gr_id', 'gene_id', 'gene_name', 
