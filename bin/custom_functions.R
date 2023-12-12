98faec432dae751fb8bb17b31bccac734a061fd7
Maria Litovchenko <maria.litovchenko@gmail.com>
2023-12-12 11:56:02 +0000

    Added columns specifying genome version of cn and multiplicity data

--- bin/custom_functions.R
+++ bin/custom_functions.R
@@ -110,7 +110,8 @@ readParticipantInventory <- function(inventoryPath, cores = 1) {
                   select = c('tumor_subtype', 'participant_id', 
                              'participant_tumor_subtype', 'somatic_path', 
                              'somatic_genome', 'cohort_name',
-                             'cn_segments_path', 'mutmultiplicity_path'),
+                             'cn_segments_path', 'cn_segments_genome',
+                             'mutmultiplicity_path', 'mutmultiplicity_genome'),
                   nThread = cores)
   result[, tumor_subtype := as.character(tumor_subtype)]
   result[, participant_tumor_subtype := as.character(participant_tumor_subtype)]
