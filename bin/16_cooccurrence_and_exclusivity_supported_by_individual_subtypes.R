3f7eb20d9c6884b6513889cd01d3e4c8237e32fd
Maria Litovchenko <maria.litovchenko@gmail.com>
2024-05-06 20:23:00 +0100

    Created figures_create_n_coding_vs_noncoding_driver_mutations_plot.R

--- bin/16_cooccurrence_and_exclusivity_supported_by_individual_subtypes.R
+++ bin/16_cooccurrence_and_exclusivity_supported_by_individual_subtypes.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-# FILE: cooccurrence_and_exclusivity.R ----------------------------------------
+# FILE: cooccurrence_and_exclusivity_supported_by_individual_subtypes.R -------
 #
 # DESCRIPTION:
 #
@@ -15,12 +15,37 @@
 # CREATED:  17.12.2021
 # REVISION: 28.01.2024
 
+# Source custom functions -----------------------------------------------------
+#' get_script_dir
+#' @description Returns parent directory of the currently executing script
+#' @author https://stackoverflow.com/questions/47044068/get-the-path-of-current-script
+#' @return string, absolute path
+#' @note this functions has to be present in all R scripts sourcing any other
+#' script. Sourcing R scripts with use of box library is unstable then multiple
+#' processes try to execute the source at the same time.
+get_script_dir <- function() {
+  cArgs <- tibble::enframe(commandArgs(), name = NULL)
+  cArgsSplit <- tidyr::separate(cArgs, col = value, into = c("key", "value"),
+                                sep = "=", fill = "right")
+  cArgsFltr <- dplyr::filter(cArgsSplit, key == "--file")
+  
+  result <- dplyr::pull(cArgsFltr, value)
+  result <- tools::file_path_as_absolute(dirname(result))
+  result
+}
+
+srcDir <- get_script_dir()
+# to spread out multiple processes accessing the same file
+Sys.sleep(sample(1:15, 1))
+source(paste0(srcDir, '/custom_functions.R'))
+
+
 # Libraries -------------------------------------------------------------------
 suppressWarnings(suppressPackageStartupMessages(library(argparse)))
 suppressWarnings(suppressPackageStartupMessages(library(data.table)))
 
 # Functions -------------------------------------------------------------------
-#' check_significance_indiv_tumorType
+#' check_significance_in_uniform_subtype
 #' @description Checks if a gene pair was found significant (based on the raw
 #' p-value) in the runs of DISCOVER made on histologically uniform tumor 
 #' subtypes.
@@ -33,9 +58,9 @@ suppressWarnings(suppressPackageStartupMessages(library(data.table)))
 #'        pair in the individual subtypes
 #' @return logical, TRUE if there was a tumor subtype which have p-value for 
 #'         that gene pair < p_val_max
-check_significance_indiv_tumorType <- function(gene_pair,
-                                               discoverUniformSubtypes, 
-                                               p_val_max = 0.1) {
+check_significance_in_uniform_subtype <- function(gene_pair,
+                                                  discoverUniformSubtypes, 
+                                                  p_val_max = 0.1) {
   gene_pair <- as.data.table(t(gene_pair))
   gene_pair <- gene_pair[, c('gr_id_1', 'gene_id_1', 'gr_id_2', 'gene_id_2', 
                              'comparison', 'mode'), with = F]
@@ -51,7 +76,7 @@ check_significance_indiv_tumorType <- function(gene_pair,
                                 'comparison', 'mode'))
   result <- F
   if (nrow(indivDiscover) != 0) {
-    if (min(indivDiscover$discover.p.value_subtypeBiasRemoved, 
+    if (min(indivDiscover$p.value_subtypeBiasRemoved, 
             na.rm = T) <= p_val_max) {
       result <- T
     }
@@ -68,14 +93,8 @@ parser$add_argument("-c", "--cancer_subtype", required = T, type = 'character',
                     help = subtypeHelp)
 
 discoverHelp <- 'Path to DISCOVER result for the composite subtype'
-parser$add_argument("-d", "--discover", required = T, 
+parser$add_argument("-d", "--discover_composite_subtype", required = T, 
                     type = 'character', help = discoverHelp)
-
-padjHelp <- paste('Cut off on adjusted p-value for significance of mutual ',
-                  'incompatibility or co-occurence in histologically ',
-                  'composite subtype')
-parser$add_argument("-padj", "--p_adj_max_comp_subtype", required = T, 
-                    type = 'character', help = padjHelp)
 
 unifSubtypeHelp <- paste('List of histologically uniform tumor subtypes from',
                          'which the composite subtype contains')
@@ -84,7 +103,7 @@ parser$add_argument("-cu", "--uniformal_tumor_subtypes", required = T,
 
 discoverUnifHelp <- paste('Paths to DISCOVER result files for the',
                           'histologically uniform subtypes')
-parser$add_argument("-du", "--discover_result_uniformal_subtypes", nargs = '+',
+parser$add_argument("-du", "--discover_uniformal_subtypes", nargs = '+',
                     required = T, type = 'character', help = discoverUnifHelp)
 
 pvalHelp <- paste('Cut off on raw p-value for significance of mutual ',
@@ -101,7 +120,7 @@ args <- parser$parse_args()
 args$p_adj_max_comp_subtype <- as.numeric(args$p_adj_max_comp_subtype)
 args$pval <- as.numeric(args$pval)
 if (length(args$uniformal_tumor_subtypes) != 
-    length(args$discover_result_uniformal_subtypes)) {
+    length(args$discover_uniformal_subtypes)) {
   stop('[', Sys.time(), '] Please submit the same number of uniformal ',
        'tumor subtypes and DISCOVER result files for uniformal tumor ',
        'subtypes.')
@@ -113,37 +132,52 @@ printArgs(args)
 
 # Test arguments --------------------------------------------------------------
 # args <- list(cancer_subtype = 'Panlung', 
-#              discover_result = '',  p_adj_max_comp_subtype = 0.1,
+#              discover_composite_subtype = '', 
 #              uniformal_tumor_subtypes = '',
-#              discover_result_uniformal_subtypes = '',
-#              p_max_unif_subtype = 0.1, output = '')
+#              discover_uniformal_subtypes = '',
+#              p_max_unif_subtype = "",
+#              output = '')
 
 # Read DISCOVER results for composite subtype ---------------------------------
-discover <- fread(args$discover_result, header = T, stringsAsFactors = F)
+discover <- fread(args$discover_composite_subtype, header = T, 
+                  stringsAsFactors = F)
 message('[', Sys.time(), '] Read DISCOVER results for composite subtype ',
-        args$cancer_subtype, ": ", args$discover_result)
+        args$cancer_subtype, ": ", args$discover_composite_subtype)
 
 # Read DISCOVER results for histologically uniform subtypes -------------------
-discoverUnifSubtypes <- lapply(args$discover_result_uniformal_subtypes, 
+discoverUnifSubtypes <- lapply(args$discover_uniformal_subtypes, 
                                fread, header = T, stringsAsFactors = F)
 message('[', Sys.time(), '] Read DISCOVER results for uniformal subtypes ',
         paste0(args$uniformal_tumor_subtypes, collapse = ', '),
-        ": ", paste0(args$discover_result_uniformal_subtypes, collapse = ', '))
+        ": ", paste0(args$discover_uniformal_subtypes, collapse = ', '))
+
+discoverUnifSubtypes <- do.call(rbind, discoverUnifSubtypes)
+discoverUnifSubtypes <- discoverUnifSubtypes[p.value_subtypeBiasRemoved <= args$p_max_unif_subtype]
+discoverUnifSubtypes <- discoverUnifSubtypes[,.(gene_id_1, gr_id_1, 
+                                                gene_id_2, gr_id_2)]
+discoverUnifSubtypes <- unique(discoverUnifSubtypes)
 
 # Check that linked genomic elements are supported by signal from histologically uniform subtypes ----
-discover[, support_by_indivTT := F]
-isSignificant <- discover$discover.p.adj_subtypeBiasRemoved <= args$p_adj_max_comp_subtype
-if (any(isSignificant, na.rm = T)) {
-  discover[isSignificant]$support_by_indivTT <- apply(discover[isSignificant], 1,
-                                                      check_significance_indiv_tumorType,
-                                                      discoverUnifSubtypes,
-                                                      args$p_max_unif_subtype)
+discover[, supported_by_individual_subtypes := F]
+if (nrow(discoverUnifSubtypes) > 0) {
+  discoverUnifSubtypes[, pair_id := apply(discoverUnifSubtypes[,.(gene_id_1,
+                                                                  gr_id_1, 
+                                                                  gene_id_2, 
+                                                                  gr_id_2)], 1,
+                                          paste, collapse = "--")]
+  discoverUnifSubtypes[, pair_id_rev := apply(discoverUnifSubtypes[,.(gene_id_1,
+                                                                      gr_id_1, 
+                                                                      gene_id_2,
+                                                                      gr_id_2)],
+                                              1, paste, collapse = "--")]
+  
+  discover[, pair_id := apply(discover[,.(gene_id_1, gr_id_1, 
+                                          gene_id_2, gr_id_2)], 1,
+                              paste, collapse = "--")]
+  supported <- discover$pair_id %in% discoverUnifSubtypes$pair_id |
+    discover$pair_id %in% discoverUnifSubtypes$pair_id_rev
+  discover[supported]$supported_by_individual_subtypes <- T
 }
-
-discover[, plotLab := '']
-discover[discover.p.adj_subtypeBiasRemoved < 0.1]$plotLab <- '*'
-discover[discover.p.adj_subtypeBiasRemoved < 0.01]$plotLab <- '**'
-discover[discover.p.adj_subtypeBiasRemoved < 0.001]$plotLab <- '***'
 
 message('[', Sys.time(), '] Updated DISCOVER results for ', 
         args$cancer_subtype, ' taking into account support or lack of ',
